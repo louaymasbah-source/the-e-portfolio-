68d4c68f84c9815b097e9f5404db49a48709d78e
louaymasbah-source <louaymasbah@gmail.com>
2025-11-02 00:07:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/the-e-portfolio/',   // << add this
+  base: '/the-e-portfolio-/',   // << add this
 })
 
